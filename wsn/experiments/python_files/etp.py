98832441cb6e17ec17efa429defe58e750007cea
Chenna Keshava B S <chenna@ualberta.ca>
2019-07-11 13:39:41 -0600

    modified etp

--- wsn/experiments/python_files/etp.py
+++ wsn/experiments/python_files/etp.py
@@ -4,11 +4,19 @@ import tensorflow as tf
 # A and B are two (N by 2) matrices
 
 def func1(p, q, count_invs):
-        ans = tf.cond(p < q, lambda: tf.add(count_invs, 1), lambda: tf.add(count_invs, 0))
+        # print('DEBUG - func1: old count_invs = {}'.format(count_invs))
+        ans = tf.cond(p < q, lambda: add_1_func(), lambda: dummy_func())
+
+        # print('DEBUG - func1: NEW count_invs = {}'.format(ans))
+
         return ans
 
 def func2(p, q, count_invs):
-        ans = tf.cond(p > q, lambda: tf.add(count_invs, 1), lambda: tf.add(count_invs, 0))
+        # print('DEBUG - func2    : old count_invs = {}'.format(count_invs))
+
+        ans = tf.cond(p > q, lambda: add_1_func(), lambda: dummy_func())
+        # print('DEBUG - func2: NEW count_invs = {}'.format(ans))
+
         return ans
 
 
@@ -16,16 +24,14 @@ def func2(p, q, count_invs):
 def dummy_func():
         return 0
 
+def add_1_func():
+        return 1
+
 
 def get_etp_without_rotation(A, B):
 
-        # print('DEBUG - get_etp_without_rotation: Shape Matrix-A : {} {}'.format(A.shape[0], A.shape[1]))
-        # print('DEBUG - get_etp_without_rotation: Shape Matrix-B : {}'.format(B.shape))
-        # A = tf.reshape(A, (-1, 2))
 
         n = A.get_shape().as_list()[0]
-        # print(type(n))
-        # print(n)
 
         A = tf.cast(A, dtype=tf.float32)
         B = tf.cast(B, dtype=tf.float32)
@@ -38,12 +44,19 @@ def get_etp_without_rotation(A, B):
                 for j in range(0, n):
 
                         # check for inversions along X-axis                        
-                        count_invs = tf.cond(A[i-1, 0] < A[j-1, 0], lambda: func1(B[j-1, 0], B[i-1, 0], count_invs), lambda: dummy_func())
-                        count_invs = tf.cond(A[i-1, 0] > A[j-1, 0], lambda: func2(B[j-1, 0], B[i-1, 0], count_invs), lambda: dummy_func())
+                        count_invs += tf.cond(A[i, 0] < A[j, 0], lambda: func1(B[j, 0], B[i, 0], count_invs), lambda: dummy_func())
+                        # print('DEBUG - MAIN: NEW count_invs = {}'.format(count_invs))
 
+                        count_invs += tf.cond(A[i, 0] > A[j, 0], lambda: func2(B[j, 0], B[i, 0], count_invs), lambda: dummy_func())
+                        # print('DEBUG - MAIN: NEW count_invs = {}'.format(count_invs))
+                        
                         # check for inversions along Y-axis
-                        count_invs = tf.cond(A[i, 1] < A[j, 1], lambda: func1(B[j, 1], B[i, 1], count_invs), lambda: dummy_func())
-                        count_invs = tf.cond(A[i, 1] > A[j, 1], lambda: func2(B[j, 1], B[i, 1], count_invs), lambda: dummy_func())
+                        count_invs += tf.cond(A[i, 1] < A[j, 1], lambda: func1(B[j, 1], B[i, 1], count_invs), lambda: dummy_func())
+                        # print('DEBUG - MAIN: NEW count_invs = {}'.format(count_invs))
+
+                        count_invs += tf.cond(A[i, 1] > A[j, 1], lambda: func2(B[j, 1], B[i, 1], count_invs), lambda: dummy_func())
+                        # print('DEBUG - MAIN: NEW count_invs = {}'.format(count_invs))
+
 
 
         count_invs/=(n * (n - 1))
@@ -57,8 +70,10 @@ def get_best_etp(A, B):
         B = tf.reshape(B, shape=(-1, 2))
 
 
+
+
         x = 0
-        final_etp = 100
+        final_etp = get_etp_without_rotation(A, B)
         for x in range(0, 360, 50):
                 rot_matrix = np.array([[np.cos(np.radians(x)), -np.sin(np.radians(x))], [np.sin(np.radians(x)), np.cos(np.radians(x))]])
                 rot_matrix = tf.convert_to_tensor(rot_matrix, dtype=tf.float32)
@@ -67,7 +82,7 @@ def get_best_etp(A, B):
                 B = tf.cast(B, dtype=tf.float32)
                 temp = get_etp_without_rotation(A, tf.matmul(B, rot_matrix))
 
-                final_etp = tf.cond(temp < final_etp, lambda: temp, lambda: tf.cast(final_etp, dtype=tf.float64))
+                final_etp = tf.cond(temp > final_etp, lambda: temp, lambda: tf.cast(final_etp, dtype=tf.float64))
 
                 # if x % 10 == 0:
                 # print('Current best of count_invs = {} at Angle {}'.format(final_etp, x))
